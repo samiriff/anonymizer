ffee8a38f2d19833b3b979e153b13f73e9d0226b
I302861 <samir.sheriff@sap.com>
2019-04-11 14:13:59 +0530

    Moved main code from anonymize.py to __main__.py and exported classes in __init__.py

---
+++ anonymizer/__init__.py
@@ -0,0 +1,2 @@
+from anonymizer.anonymize import Anonymizer
+from anonymizer.faker_type import FakerType
--- anonymizer/anonymize.py
+++ anonymizer/anonymize.py
@@ -70,14 +70,3 @@ class Anonymizer:
         elif self._faker_type == FakerType.ADDRESS:
             return self._faker.address()
         return None
-
-
-if __name__ == '__main__':
-    names = ['Kevin Bell', 'Ricky Sheppard', 'James Hill MD', 'Judith Crawford',
-             'Christopher Garza', 'Lindsey Briggs', 'Andrew Bowers', 'Tracy Tyler',
-             'Ashlee Green', 'Matthew Burch']
-    anonymizer = Anonymizer(faker_type=FakerType.NAME)
-    anonymizedNames = anonymizer.get_anonymized_names(names)
-    originalNames = anonymizer.get_original_names(anonymizedNames)
-    print("Anonymized Names = ", anonymizedNames)
-    print("Original Names = ", originalNames)
